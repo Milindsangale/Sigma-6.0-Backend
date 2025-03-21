2d160adf8229d63e49e973a693e0a9f58ee03519
Milind Sangale <smilind900@gmail.com>
2025-03-21 23:02:32 +0530

    first commit

---
+++ NodeJS/app.js
@@ -0,0 +1,32 @@
+/* let n =5;
+
+for(let i=0; i<n; i++){
+    console.log("Hello World",i);
+}
+
+
+
+console.log("milind sanga")
+
+
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+console.log("milind sanga")
+ */
+
+/* let args = process.argv;
+
+for (let i = 2; i < args.length; i++) {
+    console.log("hello to",args[i]);
+} */
+
+
+const fs = require('./fruits');
+console.log(fs);
