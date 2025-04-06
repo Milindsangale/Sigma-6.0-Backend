21a1b304da363b55ffa4f8d8faea890761f2082e
Milind Sangale <smilind900@gmail.com>
2025-04-06 19:52:21 +0530

    SQL

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,5 +1,16 @@
 const { faker } = require('@faker-js/faker');
 const mysql=require("mysql2");
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -25,23 +36,97 @@ let  getRandomUser = () => {
 ];
   };
   
-let data  =[];
+/* let data  =[];
 for (let i = 0; i < 50; i++) {
    data.push(getRandomUser());   //2000  random dta
 }
+ */
 
 
-try {
-    connection.query(q, [data], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-      });
-  } catch (err) {
-    console.error(err);
-  }
+// Home Page
+  app.get('/', (req, res) => {
+    let q = "SELECT count(*) FROM user";
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+           let count= result[0] ["count(*)"];
+           /*  res.send("milindsnaga"); */
+           res.render("home.ejs",{count});
+           /*  res.send(result[0] ["count(*)"]); */
+          });
+      } catch (err) {
+        console.error(err);
+        res.send('Error occurred while fetching data');
+      }
+    /* res.send('Welcome the home page!'); */
+  });
 
-  connection.end();
+
+  //Show Rout
+app.get('/user', (req, res) => {
+    let q = "SELECT * FROM user";
+    try {
+        connection.query(q, (err, users) => {
+            if (err) throw err;
+            /* res.send(result); */
+             res.render("user.ejs",{users}); 
+          });
+      } catch (err) {
+        console.error(err);
+        res.send('Error occurred while fetching data');
+      }
+  });
+
+
+  //EDIT ROUTE
+app.get('/user/:id/edit', (req, res) => {
+    let {id} = req.params;
+    let q = `SELECT * FROM user where id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+          let user = result[0];
+            res.render("edit.ejs", {user}); 
+          });
+      } catch (err) {
+        console.error(err);
+        res.send('Error occurred while fetching data');
+      }
+});
+
+
+//UPDATE ROUTE
+app.patch('/user/:id', (req, res) => {
+    let {id} = req.params;
+    let {password: formPass, username: newUsername} = req.body;
+
+    let q = `SELECT * FROM user where id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+          let user = result[0];
+           if (formPass != user.password) {
+            res.send("password is not correct");
+           }else {
+            //query to update the username
+            let q2 = `UPDATE user SET username='${newUsername}' where id='${id}'`;
+            connection.query(q2, (err, result) => {
+                if (err) throw err;
+                res.redirect("/user");
+               /*  res.redirect('/user'); */
+            });
+           }
+          });
+      } catch (err) {
+        console.error(err);
+        res.send('Error occurred while fetching data');
+      }
+
+});
 
 
 
-/*   console.log(getRandomUser()); */
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
