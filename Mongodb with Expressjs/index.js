577a57d0b1b0e637fdd09870976f520216bd3c00
Milind Sangale <smilind900@gmail.com>
2025-04-18 11:58:27 +0530

    Mongodb with Expressjs

--- Mongodb with Expressjs/index.js
+++ Mongodb with Expressjs/index.js
@@ -4,11 +4,15 @@ const port = 3000;
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));  // for css
+app.use(express.urlencoded({extended:true})); // for form data
+app.use(methodOverride('_method')); // for put and delete requests
+
 
 
 main()
@@ -32,6 +36,57 @@ app.get('/chats', async(req, res) => {
   });
 
 
+//new`route
+app.get('/chats/new', (req, res) => {
+    res.render('new.ejs');
+  });  
+
+  //create route
+ app.post('/chats', async (req, res) => {
+    let{ from, to, message } = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        message: message,
+        createdAt: new Date()
+    });
+    //save new chat
+   newChat.save()
+    .then(() => {
+        console.log('New chat created:', newChat);
+    })
+    .catch((err) => {
+        console.error('Error creating chat:', err);
+    });
+   res.redirect('/chats');
+  }); 
+
+//edit route
+app.get('/chats/:id/edit', async(req, res) => {
+    let { id } = req.params;
+    let chat = Chat.findById(id);
+    res.render('edit.ejs', {chat});
+  });
+
+//update route
+app.put('/chats/:id', async(req, res) => {
+    let { id } = req.params;
+   let {message: newMsg} = req.body;
+   let  updatechat= await Chat.findByIdAndUpdate(
+    id, {message: newMsg},
+        {new: true, runValidators: true}
+    );
+    console.log(updatechat);
+   res.redirect('/chats'); 
+  });
+
+  //delete route
+app.delete('/chats/:id', async(req, res) => {
+    let { id } = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect('/chats');
+  });
+  
 app.get('/', (req, res) => {
   res.send('root is working!')
 });
--- Mongodb with Expressjs/models/chat.js
+++ Mongodb with Expressjs/models/chat.js
@@ -13,7 +13,7 @@ const chatSchema = new mongoose.Schema({
     },
     message:{
         type:String,
-        maxLength:50,
+        maxLength:10000,
         
     },
     createdAt:{
