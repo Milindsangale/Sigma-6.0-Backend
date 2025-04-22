586d8ae13b18259363e943d042ea13041c386df1
Milind Sangale <smilind900@gmail.com>
2025-04-23 01:30:43 +0530

    Middleware

--- Middleware Backend/app.js
+++ Middleware Backend/app.js
@@ -3,18 +3,40 @@ const app= expess();
 
 //midleware
 
-app.use((req,res,next)=>{
+/* app.use((req,res,next)=>{
     console.log("Middleware is running")
-    /* res.send("Middleware is running") */
+ 
     next();
     console.log("this is after next()")
-    // this will not run if next() is called
+    
 });
 app.use((req,res,next)=>{
     console.log("Middleware is running 2")
     next();
 
+}); */
+//logging middleware
+/* app.use((req,res,next)=>{
+    req.time = new Date().toString();
+    console.log("Middleware is running",req.method ,req.path, req.time);
+    next();
+}); */
+
+app.use("/api",(req,res,next)=>{
+   let {token} = req.query;
+   if(token == "givemeacookie"){
+       next();
+    }
+    else{
+         res.status(401).send("Unauthorized")
+    }
 });
+
+
+app.get('/api',(req,res)=>{
+    res.send("Middleware is running")
+}   );
+
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
@@ -23,6 +45,11 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.send("About Page")
 });
+
+//page for not found
+app.use((req,res,next)=>{
+    res.status(404).send("Page not found")
+}); 
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
